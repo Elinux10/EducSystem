08c209cfc21cac7d46e9931aa0e34af0828a67b6
PabloHenrique06 <pablo_blackside@hotmail.com>
2016-08-16 12:21:48 -0300

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>EducSystem</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body id="fundo">
+	<header id="cabecalho">
+       <div id="nome"><h1>EducSystem</h1> </div>
+  	   <div id="menu">
+    		<nav id="itensMenu">
+            	<ul>
+                	<li><a href="#">Professores</a></li>
+                    <li><a href="">Alunos</a></li>
+                    <li><a href="">Disciplinas</a></li>
+                    <li><a href="">Notas</a></li>
+                </ul>
+        	</nav>
+       </div>
+       <div id="dadosUser">
+       	   		Bem Vindo, Usuario<br>
+				Disciplina<br>
+                <a href="">Loggof</a>		
+       </div>
+	</header>
+    
+    <section>
+    	
+    </section>
+    
+    
+</body>
+</html>
